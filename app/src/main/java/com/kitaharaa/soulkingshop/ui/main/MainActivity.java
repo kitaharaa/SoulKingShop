71645caf8840a6ddbf6580c0ac81fe3e196e68b3
amogus3000 <bvalentin1415@gmail.com>
2022-07-13 23:35:55 +0300

    added MVP folder structure and View/Presenter managing interfaces

--- app/src/main/java/com/kitaharaa/soulkingshop/ui/main/MainActivity.java
+++ app/src/main/java/com/kitaharaa/soulkingshop/ui/main/MainActivity.java
@@ -1,8 +1,10 @@
-package com.kitaharaa.soulkingshop;
+package com.kitaharaa.soulkingshop.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.kitaharaa.soulkingshop.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/kitaharaa/soulkingshop/ui/main/MainPresenter.java
@@ -0,0 +1,4 @@
+package com.kitaharaa.soulkingshop.ui.main;
+
+public interface MainPresenter {
+}
