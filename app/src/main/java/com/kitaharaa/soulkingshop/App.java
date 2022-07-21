844099d1f419802a3f37e9f4b7cb3221f2c3af3f
amogus3000 <bvalentin1415@gmail.com>
2022-07-21 22:36:54 +0300

    added App.class

---
+++ app/src/main/java/com/kitaharaa/soulkingshop/App.java
@@ -0,0 +1,23 @@
+package com.kitaharaa.soulkingshop;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    private static App sInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        init(this);
+    }
+
+    private static void init(App app) {
+        sInstance = app;
+    }
+
+    public static App getInstance() {
+        return sInstance;
+    }
+
+}
